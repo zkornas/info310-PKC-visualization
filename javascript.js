ed293d973eca5bbf3e82be6d8be373ce0f0ed4b0
zkornas <zkornas@uw.edu>
2023-12-24 12:55:28 -0800

    Utilized objects to handle each collor section

--- javascript.js
+++ javascript.js
@@ -35,92 +35,108 @@ alicePrivateColorInSharedSecret.style.background = alicePrivateColor;
 
 var bobPublicColorInSharedSecret = document.getElementById('bobPublicColorInSharedSecret');
 
-// Function for handeling user-selected color for Bob and Alice's private colors
-// Parameters
-//      colorPicker: The actual color picker element
-//      name: 'alice', 'bob', or 'public' so that we know which color variable to update
-//      miniDiv: The mini-div that shows Bob/Alice's private color in the equation to make their public color
-//      inSharedSecret: The mini-div that shows Alice/Bob's private color in the equationto make the shared secret color
+// Constructor for making a new color object. We will have three: Alice, Bob, and Public
+function colorConstructor(picker, colorValue, miniDivOne, miniDivTwo, personalPublicColor, personalPublicColorDiv, publicColorInSharedSecretDiv, sharedSecretColor, sharedSecretColorDiv, generatePublicColorButton, generateSharedSecretButton, showHideButton, colorsDiv, isHidden) {
+    this.picker = picker;
+    this.colorValue = colorValue;
+    this.miniDivOne = miniDivOne;
+    this.miniDivTwo = miniDivTwo;
 
-function handleColorPickerInput(colorPicker, name, miniDiv, inSharedSecret) {
-    var selectedColor = colorPicker.value;
-
-    if (name == 'alice') {
-        alicePrivateColor = selectedColor;
-    } else if (name == 'bob') {
-        bobPrivateColor = selectedColor;
-    } else {
-        publicColor = selectedColor;
-    }
-    miniDiv.style.background = selectedColor;
-    inSharedSecret.style.background = selectedColor;
-}
-
-// Event Listener for when the user selects Alice's private color
-aliceColorPicker.addEventListener('input', function () {
-    handleColorPickerInput(aliceColorPicker, 'alice', alicePrivateColorMiniDiv, alicePrivateColorInSharedSecret);
-    console.log('Alice private color: ', alicePrivateColor);
-});
-
-// Event Listener for when the user selects Bob's private color
-bobColorPicker.addEventListener('input', function () {
-    handleColorPickerInput(bobColorPicker, 'bob', bobPrivateColorMiniDiv, bobPrivateColorInSharedSecret);
-    console.log('Bob private color: ', bobPrivateColor);
-});
-
-// Event Listener for when the user selects public color
-publicColorPicker.addEventListener('input', function () {
-    handleColorPickerInput(publicColorPicker, 'public', publicColorMiniDivAlice, publicColorMiniDivBob);
-});
-
-// generate alice public color
-var generateAlicePublicColorButton = document.getElementById('generateAlicePublicColor');
-var alicePublicColorDiv = document.getElementById('alicePublicColor');
-
-generateAlicePublicColorButton.addEventListener('click', function () {
-    generatePublicColor(alicePublicColor, alicePrivateColor, alicePublicColorDiv, alicePublicColorInSharedSecret);
-});
-
-// generate bobs public color
-var generateBobPublicColorButton = document.getElementById('generateBobPublicColor');
-var bobPublicColorDiv = document.getElementById('bobPublicColor');
-
-generateBobPublicColorButton.addEventListener('click', function() {
-    generatePublicColor(bobPublicColor, bobPrivateColor, bobPublicColorDiv, bobPublicColorInSharedSecret);
-});
-
-// Function to generate Alice/Bob's public color by mixing their private color with the public color
-// Parameters
-//      personalPublicColor: The individual's personal public color that can be used by anyone. 
-//                           Is assigned a value after calling mixColorsWithRatio function.
-//      privateColor: The individual's private color.
-//      publicColorDiv: The div element that will display the public color.
-//      personalPublicColorInSharedSecret: The div element that display's the individual's public color
-//                                         in the equation used to generate the shared secret color.
-function generatePublicColor(personalPublicColor, privateColor, publicColorDiv, personalPublicColorInSharedSecret) {
-    personalPublicColor = mixColorsWithRatio(privateColor, publicColor, 1, 1);
-    publicColorDiv.style.backgroundColor = personalPublicColor;
-    personalPublicColorInSharedSecret.style.background = personalPublicColor;
+    // These will only be defined for Alice and Bob.
+    this.personalPublicColor = personalPublicColor;
+    this.personalPublicColorDiv = personalPublicColorDiv;
+    this.publicColorInSharedSecretDiv = publicColorInSharedSecretDiv;
+    this.sharedSecretColor = sharedSecretColor;
+    this.sharedSecretColorDiv = sharedSecretColorDiv;
+    this.generatePublicColorButton = generatePublicColorButton;
+    this.generateSharedSecretButton = generateSharedSecretButton;
+    this.showHideButton = showHideButton;
+    this.colorsDiv = colorsDiv;
+    this.isHidden = isHidden;
 };
 
-// mix alice's private color with bob's public color
-var generateSharedSecretColorAliceButton = document.getElementById('generateSharedSecretColorAlice');
-var sharedSecretColorAliceDiv = document.getElementById('aliceSharedPrivateColor');
+var alice = new colorConstructor(
+    document.getElementById('aliceColorPicker'),
+    document.getElementById('aliceColorPicker').value,
+    document.getElementById('alicePrivateColorInPublicColor'),
+    document.getElementById('alicePrivateColorInSharedSecret'),
+    undefined,
+    document.getElementById('alicePublicColor'),
+    document.getElementById('alicePublicColorInSharedSecret'),
+    undefined,
+    document.getElementById('aliceSharedPrivateColor'),
+    document.getElementById('generateAlicePublicColor'),
+    document.getElementById('generateSharedSecretColorAlice'),
+    document.getElementById('aliceOnLaptop'),
+    document.getElementById('aliceColors'),
+    true
+)
 
-generateSharedSecretColorAliceButton.addEventListener('click', function() {
+var bob = new colorConstructor(
+    document.getElementById('bobColorPicker'),
+    document.getElementById('bobColorPicker').value,
+    document.getElementById('bobPrivateColorInPublicColor'),
+    document.getElementById('bobPrivateColorInSharedSecret'),
+    undefined,
+    document.getElementById('bobPublicColor'),
+    document.getElementById('bobPublicColorInSharedSecret'),
+    undefined,
+    document.getElementById('bobSharedPrivateColor'),
+    document.getElementById('generateBobPublicColor'),
+    document.getElementById('generateSharedSecretColorBob'),
+    document.getElementById('bobOnLaptop'),
+    document.getElementById('bobColors'),
+    true
+)
 
-    aliceSharedSecretColor = mixColorsWithRatio(alicePrivateColor, bobPublicColor, 1, 2);
-    sharedSecretColorAliceDiv.style.backgroundColor = aliceSharedSecretColor;
+var public = new colorConstructor(
+    document.getElementById('publicColorPicker'),
+    document.getElementById('publicColorPicker').value,
+    document.getElementById('publicColorAlice'),
+    document.getElementById('publicColorBob')
+)
+
+function handleColorPickerInput(colorObject) {
+   var selectedColor = colorObject.picker.value;
+   colorObject.colorValue = selectedColor;
+   colorObject.miniDivOne.style.background = selectedColor;
+   colorObject.miniDivTwo.style.background = selectedColor;
+};
+
+alice.picker.addEventListener('input', function() {
+    handleColorPickerInput(alice);
 });
 
-// mix bob's private color with alice's public color
-var generateSharedSecretColorBobButton = document.getElementById('generateSharedSecretColorBob');
-var sharedSecretColorBobDiv = document.getElementById('bobSharedPrivateColor');
+bob.picker.addEventListener('input', function() {
+    handleColorPickerInput(bob);
+});
 
-generateSharedSecretColorBobButton.addEventListener('click', function() {
+public.picker.addEventListener('input', function() {
+    handleColorPickerInput(public);
+});
 
-    bobSharedSecretColor = mixColorsWithRatio(bobPrivateColor, alicePublicColor, 1, 2);
-    sharedSecretColorBobDiv.style.backgroundColor = bobSharedSecretColor;
+alice.generatePublicColorButton.addEventListener('click', function() {
+    generatePublicColor(alice);
+})
+
+bob.generatePublicColorButton.addEventListener('click', function() {
+    generatePublicColor(bob);
+});
+
+function generatePublicColor(colorObject) {
+    colorObject.personalPublicColor = mixColorsWithRatio(colorObject.colorValue, public.colorValue, 1, 1);
+    colorObject.personalPublicColorDiv.style.background = colorObject.personalPublicColor;
+    colorObject.publicColorInSharedSecretDiv.style.background = colorObject.personalPublicColor;
+};
+
+alice.generateSharedSecretButton.addEventListener('click', function() {
+    alice.sharedSecretColor = mixColorsWithRatio(alice.colorValue, bob.personalPublicColor, 1, 2);
+    alice.sharedSecretColorDiv.style.backgroundColor = alice.sharedSecretColor;
+});
+
+bob.generateSharedSecretButton.addEventListener('click', function() {
+    bob.sharedSecretColor = mixColorsWithRatio(bob.colorValue, alice.personalPublicColor, 1, 2);
+    bob.sharedSecretColorDiv.style.backgroundColor = bob.sharedSecretColor;
 });
 
 function mixColorsWithRatio(color1, color2, ratio1, ratio2) {
@@ -149,62 +165,67 @@ function componentToHex(c) {
     return hex.length == 1 ? '0' + hex : hex;
 }
 
-var aliceOnLaptopButton = document.getElementById('aliceOnLaptop');
-var aliceColorsDiv = document.getElementById('aliceColors');
-var alicePublicColorEquationDiv = document.getElementById('alicePublicColorEquation');
-
-aliceOnLaptopButton.addEventListener('click', function() {
-if (aliceOnLaptopButton.src.includes('alice_happy_laptop.PNG')){
-    aliceOnLaptopButton.src = 'imgs/alice_neutral_laptop.PNG';
-    aliceColorsDiv.classList.add('blur');
-    alicePrivateColorMiniDiv.classList.add('grey');
-} else {
-    aliceOnLaptopButton.src = 'imgs/alice_happy_laptop.PNG';
-    aliceColorsDiv.classList.remove('blur');
-    alicePrivateColorMiniDiv.classList.remove('grey');
-}
+alice.showHideButton.addEventListener('click', function(){
+    if (alice.isHidden == false) {
+        alice.showHideButton.src = 'imgs/alice_neutral_laptop.PNG';
+    } else {
+        alice.showHideButton.src = 'imgs/alice_happy_laptop.PNG';
+    }
+    showHideColorSection(alice);
 });
 
-var bobOnLaptopButton = document.getElementById('bobOnLaptop');
-var bobColorsDiv = document.getElementById('bobColors');
-var bobPublicColorEquationDiv = document.getElementById('bobPublicColorEquation');
-
-bobOnLaptopButton.addEventListener('click', function() {
-if (bobOnLaptopButton.src.includes('bob_happy_laptop.PNG')){
-    bobOnLaptopButton.src = 'imgs/bob_neutral_laptop.PNG';
-    bobColorsDiv.classList.add('blur');
-    bobPrivateColorMiniDiv.classList.add('grey');
-} else {
-    bobOnLaptopButton.src = 'imgs/bob_happy_laptop.PNG';
-    bobColorsDiv.classList.remove('blur');
-    bobPrivateColorMiniDiv.classList.remove('grey');
-}
+bob.showHideButton.addEventListener('click', function(){
+    if (bob.isHidden == false) {
+        bob.showHideButton.src = 'imgs/bob_neutral_laptop.PNG';
+    } else {
+        bob.showHideButton.src = 'imgs/bob_happy_laptop.PNG';
+    }
+    showHideColorSection(bob);
 });
+
+function showHideColorSection(colorObject) {
+    if (colorObject.isHidden == false) {
+        colorObject.colorsDiv.classList.add('blur');
+        colorObject.miniDivOne.classList.add('grey');
+        colorObject.isHidden = true;
+    } else {
+        colorObject.colorsDiv.classList.remove('blur');
+        colorObject.miniDivOne.classList.remove('grey');
+        colorObject.isHidden = false;
+    }
+};
 
 var randomButton = document.getElementById('randomButton');
 
-randomButton.addEventListener('click', function() {
+randomButton.addEventListener('click', async function() {
     randomButton.classList.add('shake-animation');
     
-    alicePrivateColor = getRandomHexColor();
-    aliceColorPicker.value = alicePrivateColor;
-    alicePrivateColorMiniDiv.style.background = alicePrivateColor;
-    alicePrivateColorInSharedSecret.style.background = alicePrivateColor;
-    
-    bobPrivateColor = getRandomHexColor();
-    bobColorPicker.value = bobPrivateColor;
-    bobPrivateColorMiniDiv.style.background = bobPrivateColor;
-    bobPrivateColorInSharedSecret.style.background = bobPrivateColor;
+    alice.colorValue = getRandomHexColor();
+    alice.picker.value = alice.colorValue;
+    alice.miniDivOne.style.backgroundColor = alice.colorValue;
+    alice.miniDivTwo.style.backgroundColor = alice.colorValue;
+    await sleep(100);
 
-    publicColor = getRandomHexColor();
-    publicColorPicker.value = publicColor;
-    publicColorMiniDivAlice.style.background = publicColor;
-    publicColorMiniDivBob.style.background = publicColor;
+    bob.colorValue = getRandomHexColor();
+    bob.picker.value = bob.colorValue;
+    bob.miniDivOne.style.backgroundColor = bob.colorValue;
+    bob.miniDivTwo.style.backgroundColor = bob.colorValue;
+    await sleep(100);
 
-    generateAlicePublicColorButton.click();
-    generateBobPublicColorButton.click();
-    generateSharedSecretColorAliceButton.click();
-    generateSharedSecretColorBobButton.click();
+    public.colorValue = getRandomHexColor();
+    public.picker.value = public.colorValue;
+    public.miniDivOne.style.backgroundColor = public.colorValue;
+    public.miniDivTwo.style.backgroundColor = public.colorValue;
+    await sleep(100);
+
+    alice.generatePublicColorButton.click();
+    await sleep(100);
+
+    bob.generatePublicColorButton.click();
+    await sleep(100);
+
+    alice.generateSharedSecretButton.click();
+    bob.generateSharedSecretButton.click();
 
     setTimeout(() => {
         randomButton.classList.remove('shake-animation');
@@ -216,7 +237,11 @@ function getRandomHexColor() {
     const letters = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+}
+
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+};
